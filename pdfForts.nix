e4f3860d5451c030c7ae2e085270bd40dee5d41b
sjau <github.com@sjau.ch>
2017-08-28 21:48:23 +0200

    sha256

--- pdfForts.nix
+++ pdfForts.nix
@@ -4,8 +4,8 @@ stdenv.mkDerivation {
 # Switch between local testing and using proper git repo
   src = fetchgit {
     url = https://github.com/sjau/pdfForts.git;
-    rev = "69f73de55fda09082025958ca21efe681c8107d8";
-    sha256 = "1nj04i52hzxvywrc3rmrb4dp4hf0wn2g8lwgr5p9asbw6x4cznrv";
+    rev = "e6f11cac0e9db48a0d188cb5074a6305876adda0";
+    sha256 = "0wbpn1jgxxfm4iy7dj48l73n90mgj3mssglqrrrzjp87r8dw3bwx";
   };
 #  src = /home/hyper/Desktop/git-repos/pdfForts;
   installPhase = ''
